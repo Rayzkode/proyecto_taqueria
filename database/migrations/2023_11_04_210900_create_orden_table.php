3bf2999d18196bd90a2b53eaf643fa350f1154c9
HectorDev <hector.dev8@outlook.com>
2023-11-11 13:42:33 -0700

    Correccion de base de datos

--- database/migrations/2023_11_04_210900_create_orden_table.php
+++ database/migrations/2023_11_04_210900_create_orden_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orden', function (Blueprint $table) {
             $table->id();
             $table->date('fecha');
+            $table->integer('mesa');
             $table->decimal('total', 10, 2);
             $table->unsignedBigInteger('suc_id');
             
